686e5bdf3dca41a1f2ccf506cff73210d5c08636
therohithborana <boranarohith07@gmail.com>
2025-04-25 15:34:52 +0530

    changes

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -8,7 +8,7 @@ export const DATA = {
   location: "India",
   locationLink: "",
   description:
-    "20 | Building BandhuAI",
+    "20 • Building PalAI",
   summary: `I build cool stuff to solve personal problems(atleast looks cool to me).
 
   I like to produce music on weekends. \n
